4fcc384c77783d1a42c8305fe607d2580fe57da9
Leonid Shevtsov <leonid@shevtsov.me>
2011-01-24 11:35:05 +0200

    design improvements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,13 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    if user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+      redirect_to root_url
+    else
+      user = User.create_with_omniauth(auth)
+      redirect_to edit_user_url
+    end
     session[:user_id] = user.id
-    redirect_to root_url
   end
 
   def destroy
