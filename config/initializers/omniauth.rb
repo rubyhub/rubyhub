c289246ff078896f082a3ad490eaaa36ff1d35b3
Leonid Shevtsov <leonid@shevtsov.me>
2011-01-24 15:43:07 +0200

    added google and twitter authorisation

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  twitter_config = YAML.load_file('config/twitter.yml')
+
   provider :open_id, OpenID::Store::Filesystem.new(File.join(Rails.root,'tmp'))
+  provider :open_id, OpenID::Store::Filesystem.new(File.join(Rails.root,'tmp')), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :twitter, twitter_config['auth_consumer_key'], twitter_config['auth_consumer_secret']
 end
