da5459a82284a983857ad50ae081179e9d9718b6
Leonid Shevtsov <leonid@shevtsov.me>
2011-01-25 23:42:48 +0200

    allow blank urls in blog

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,7 @@ class User < ActiveRecord::Base
   validates :heard_about_ruby_year, :numericality => {:greater_than => 1900, :less_than => 2010, :allow_blank => true}
   validates :started_ruby_year, :numericality => {:greater_than => 1900, :less_than => 2010, :allow_blank => true}
   
-  validates_format_of :url, :with => /^(http|https):\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
-  validates_uniqueness_of :url
+  validates_format_of :url, :with => /^(http|https):\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix, :allow_blank => tru
 
   attr_protected :provider, :uid
 
