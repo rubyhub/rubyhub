da5b09d94f6e8f420b1098b2fa46da6fdbc24b2b
Leonid Shevtsov <leonid@shevtsov.me>
2011-03-09 14:24:11 +0200

    a lot of refactoring and code cleanup connected to blogs update

---
+++ lib/http_helper.rb
@@ -0,0 +1,24 @@
+require 'net/http'
+
+module HttpHelper
+  def self.follow_redirects(url, options = {}, depth=0)
+    return_type = options.fetch(:return, :url)
+
+    uri = URI.parse(url)
+    response = nil
+
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      response = http.send((return_type==:url ? :head : :get) ,uri.path.blank? ? '/' : uri.path)
+    end
+
+    if (response.code.to_s[0,1]=='3') && (depth<5) # redirect
+      return follow_redirects(response['location'], options, depth+1)
+    else
+      return (return_type==:url) ? url : response
+    end
+  end
+
+  def self.expand_url(url)
+    follow_redirects(url, :return => :url)
+  end
+end
