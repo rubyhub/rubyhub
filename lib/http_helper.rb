5b091f7d36a6ec898d701c871a426e99f3168d21
Leonid Shevtsov <leonid@shevtsov.me>
2011-03-09 14:33:35 +0200

    a quick fix for follow redirects

--- lib/http_helper.rb
+++ lib/http_helper.rb
@@ -7,12 +7,19 @@ module HttpHelper
     uri = URI.parse(url)
     response = nil
 
+
+
     Net::HTTP.start(uri.host, uri.port) do |http|
-      response = http.send((return_type==:url ? :head : :get) ,uri.path.blank? ? '/' : uri.path)
+      response = http.send((return_type==:url ? :head : :get), uri.path.blank? ? '/' : uri.path)
     end
 
     if (response.code.to_s[0,1]=='3') && (depth<5) # redirect
-      return follow_redirects(response['location'], options, depth+1)
+      location = response['location']
+      if location.starts_with? '/'
+        location = "#{uri.scheme}://#{uri.host}:#{uri.port}#{location}"
+      end
+
+      return follow_redirects(location, options, depth+1)
     else
       return (return_type==:url) ? url : response
     end
