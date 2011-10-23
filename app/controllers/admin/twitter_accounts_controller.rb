bb833b876e8f21c6d3f4bca6aa505d5a50f0fe39
Leonid Shevtsov <leonid@shevtsov.me>
2011-10-23 11:37:15 +0300

    twitter account creation from admin (got tired/bored on rubyhub)

--- app/controllers/admin/twitter_accounts_controller.rb
+++ app/controllers/admin/twitter_accounts_controller.rb
@@ -10,4 +10,15 @@ class Admin::TwitterAccountsController < Admin::BaseController
     redirect_to admin_twitter_accounts_url
   end
 
+  def create
+    @twitter_account = TwitterAccount.new(params[:twitter_account])
+    @twitter_account.status = :active
+
+    unless @twitter_account.save
+      flash[:error] = @twitter_account.errors.map{|k,v| [k,v].join(' ')}.join(', ')
+    end
+
+    redirect_to :action => :index
+  end
+
 end
