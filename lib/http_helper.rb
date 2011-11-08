eedd93f7126b6d7b4483340d36416d0340e8b360
Leonid Shevtsov <leonid@shevtsov.me>
2011-11-08 20:31:46 +0200

    rescue socket error in http helper

--- lib/http_helper.rb
+++ lib/http_helper.rb
@@ -12,7 +12,7 @@ module HttpHelper
         Net::HTTP.start(uri.host, uri.port) do |http|
           response = http.send((return_type==:url ? :head : :get), uri.path.blank? ? '/' : uri.path)
         end
-      rescue EOFError, Net::HTTPBadResponse, Errno::ETIMEDOUT
+      rescue SocketError, EOFError, Net::HTTPBadResponse, Errno::ETIMEDOUT
         response = nil
       end
     end
